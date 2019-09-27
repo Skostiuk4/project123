def4197f017a83a5b199060e51a41935f563eb55
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-27 12:58:18 +0300

    second commit

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-    for($i=1;$i<=8;$i++) {
-        for($j=1;$j<=$i;$j++) {
-            echo $j;
-        }
-        echo "</br>";
-    }
+function stairs($arg1, $arg2) {
+for($i=$arg1;$i<=$arg2;$i++) 
+{ 
+for($j=1;$j<=$i;$j++)
+echo $j;
+{
+}
+echo '<br>';
+}
+}
+stairs(1, 50); 
 ?>
