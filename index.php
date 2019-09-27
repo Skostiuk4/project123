8c7874ecf7bf3baa38187c38f0961579259ef5d5
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-27 18:05:09 +0300

    Added third variable to stairs function

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
  	
   for ($i = $arg1; $i <= $arg2; $i++) {
    for ($j = 1; $j <= $i; $j ++) {
-     echo $j . '+';
+     echo $j . $operation;
    }
    echo '<br>';
   }
  }
-stairs(1, 7, '+');
+stairs(1, 7, '-');
 
 ?>
